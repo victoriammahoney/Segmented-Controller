fcb6752f6134852eaa3ce93c0d719a74cde14061
Victoria Mahoney <63237@psdschools.org>
2022-02-24 13:34:53 -0700

    working segmented controller

--- Segmented Controller/ViewController.swift
+++ Segmented Controller/ViewController.swift
@@ -8,10 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var SegmentedLabel: UILabel!
+    
+    @IBOutlet weak var SegmentedController: UISegmentedControl!
+    
+    @IBAction func segmentChanged(_ sender: Any) {
+        
+        //helloLabel.isHidden = false
+        switch SegmentedController.selectedSegmentIndex {
+        case 0: helloLabel.text = "Hello"
+            
+        case 1: helloLabel.text = "Bonjour"
+            
+        case 2: helloLabel.text = "Dzien Dobry"
+            
+        default: helloLabel.text = "Hello"
+        }
+        
+    }
+    @IBOutlet weak var helloLabel: UILabel!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        //helloLabel.isHidden = true
+
     }
 
 
